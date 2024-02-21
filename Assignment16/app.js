cb494d9bd9ca40c14c3db5c085e8f2a591d31d7e
Saifullah-Yousuf <sobiayousuf2050@gmail.com>
2024-02-22 03:11:16 +0500

    Assignment16 Done

---
+++ Assignment16/app.js
@@ -0,0 +1,75 @@
+//              chapter#01  "ALERT"
+// Q:1
+var Firstname = "Sobia";
+var Lastname = "Sehar";
+var Email = "abc@gmail.com";
+var Phonenumber = "xxxxxxxxxxx";
+var Password = 123456789;
+
+alert (Firstname)
+alert (Lastname)
+alert (Email)
+alert (Phonenumber)
+alert (Password)
+
+// Q:2
+alert ("You're learning JavaScript!")
+
+// Q:3
+alert ("Welcome to this website!")
+          //    Chapter#02  "(Variables for string)"
+
+// Q:1
+var myFavoriteColor = "blue";
+document.write(myFavoriteColor + "<br>")
+
+// Q:2
+var statement=" this is my page";
+alert (statement);
+document.write(statement + "<br>");
+console.log(statement);
+
+// Q:3
+var teamname = "my awsome team";
+alert (teamname);
+
+// Q:4
+var bestMan = "Charlie";
+var bestMan = "john"
+
+
+//           Chapter#3 "(Variables for numbers)"
+// Q:1
+var caseQty = 200
+console.log(caseQty)
+
+// Q:2
+caseQty = 144;
+alert (caseQty);
+console.log (caseQty);
+document.write (" ",caseQty, "<br>");
+
+// Q:3
+var num = "9";
+var num = 9;
+
+// Q:4
+var sum = 5+7;
+alert (sum);
+document.write (sum, "<br>");
+console.log (sum);
+
+// Q:5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+console.log(orderTotal);
+document.write (" ", orderTotal, "<br>");
+
+// Q:6
+var myNumber = 10;
+myNumber += 5;
+
+console.log (myNumber);
+document.write (myNumber);
---
+++ Class Practice 1/app.js
@@ -0,0 +1,97 @@
+//      *chapter#1*
+
+Q:1
+alert ("welcome, to, our, website");
+
+// Q:2
+alert ("Error! please enter a valid password");
+
+// Q:3
+alert ("welcome to JS land... \n happy Coding!");
+
+// Q:4
+alert ("welcome to JS land");
+alert ("Happy Coding! \n prevent this page from creating additional dialogs");
+
+
+// Q:5
+alert ("Hello... I can run JS through my web browser's console")
+
+// Q:6
+
+
+
+
+//        *chapter#2*
+// Q:1
+var username = ("sobia");
+document.write("<br>", username);
+
+// Q:2
+var myname =(" sobia");
+document.write("<br>", myname);
+
+// Q:3 part a)
+var message = "jsstart";
+document.write("<br>", message);
+
+// Q:3 part b)
+message = "Hello, world";
+
+// Q:3 part c)
+alert(message);
+
+// Q:4
+var firstname ="Sobia";
+var lastname = "Sehar";
+var age = 20;
+var country = "Pakistani";
+
+alert (firstname);
+alert (lastname);
+alert (age);
+alert (country);
+
+// Q:5
+var a="PIZZA";
+var b="PIZZ";
+var c="PIZ";
+var d="PI";
+var e="P";
+
+alert (a);
+alert (b);
+alert (c);
+alert (d);
+alert (e);
+
+// Q:6
+var email="sobiayousuf@gmail.com";
+
+alert (email);
+
+// Q:7
+var book = "A smarter way to learn JavaScript!";
+
+alert ("I am trying to learn from the Book"+ " " + book);
+
+// Q:9
+var myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert (myString);
+
+//         *Chapter#3*
+// Q:1
+var age="I am 15 years old";
+alert (age);
+
+// Q:3
+var birthyear=" my birth year is 2000";
+document.write("<br>", birthyear);
+
+// Q:4 part a)
+var Name=" john";
+var Product=" ordered";
+var Quantity=" 5 T-shirt(s)";
+var Brand=" on XYZ clothing store";
+
+document.write ("<br>", Name + " " + Product +  " " + Quantity +  " " + Brand )
